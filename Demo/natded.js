b2f52a6473a765ada24b69043a2af362b0363b78
Brian Howard <bhoward@depauw.edu>
2023-11-23 21:56:47 -0500

    First test of component with custom math-field

---
+++ Demo/natded.js
@@ -0,0 +1,274 @@
+import * as MathLive from "//unpkg.com/mathlive?module";
+
+// TODO put things in classes, create some components, and export them all
+
+const mf = document.getElementById("test");
+const out = document.getElementById("out");
+
+const logicShortcuts = {
+    "->": "\\rightarrow",
+    "vv": "\\lor",
+    "||": "\\lor",
+    "!!": "\\lnot",
+    "not": "\\lnot",
+    "!EE": "\\lnot\\exists",
+    "!exists": "\\lnot\\exists",
+    "implies": "\\rightarrow",
+    "→": "\\rightarrow",
+    "FF": "\\bot",
+};
+
+mathVirtualKeyboard.layouts = [
+    {
+        label: "logic",
+        tooltip: "logic operators",
+        rows: [
+            [
+                "\\land", "\\lor", "\\lnot", "\\rightarrow", "(", ")",
+            ],
+            [
+                "\\forall", "\\exists", "\\top", "\\bot", "#@_{#?}",
+            ],
+        ],
+    },
+    "alphabetic"
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+    out.innerText = "$$" + render(parse(mf.getValue("ascii-math"))) + "$$";
+    MathLive.renderMathInDocument();
+    mf.inlineShortcuts = {
+        ...mf.inlineShortcuts,
+        ...logicShortcuts,
+    };
+});
+
+mf.addEventListener("change", (event) => {
+    out.innerText = "$$" + render(parse(mf.getValue("ascii-math"))) + "$$";
+    MathLive.renderMathInElement(out);
+});
+
+function parse(s) {
+    let errors = [];
+    let [e, rest] = parseExpr(s.trim(), errors);
+    if (rest !== "") {
+        errors.push("Excess input '" + rest + "'");
+    }
+    console.log(errors);
+    if (errors.length === 0) {
+        return e;
+    } else {
+        return "E"; // TODO
+    }
+}
+
+function match(token, s, errors) {
+    if (s.startsWith(token)) {
+        return s.substring(token.length).trim();
+    } else {
+        errors.push(`Expected '${token}'; found '${s}'`);
+        return s;
+    }
+}
+
+function parseExpr(s, errors) {
+    let [e1, rest] = parseOExpr(s, errors);
+    if (rest.startsWith("→")) {
+        rest = match("→", rest, errors);
+        let [e2, rest2] = parseExpr(rest, errors);
+        e1 = { op: "implies", e1, e2 };
+        rest = rest2;
+    } else if (rest.startsWith("->")) {
+        rest = match("->", rest, errors);
+        let [e2, rest2] = parseExpr(rest, errors);
+        e1 = { op: "implies", e1, e2 };
+        rest = rest2;
+    }
+    return [e1, rest.trim()];
+}
+
+function parseOExpr(s, errors) {
+    let [e1, rest] = parseAExpr(s, errors);
+    while (rest.startsWith("or")) {
+        rest = match("or", rest, errors);
+        let [e2, rest2] = parseAExpr(rest, errors);
+        e1 = { op: "or", e1, e2 };
+        rest = rest2;
+    }
+    return [e1, rest.trim()];
+}
+
+function parseAExpr(s, errors) {
+    let [e1, rest] = parseQExpr(s, errors);
+    while (rest.startsWith("and")) {
+        rest = match("and", rest, errors);
+        let [e2, rest2] = parseQExpr(rest, errors);
+        e1 = { op: "and", e1, e2 };
+        rest = rest2;
+    }
+    return [e1, rest.trim()];
+}
+
+function parseQExpr(s, errors) {
+    if (s.startsWith("∀")) {
+        let s2 = match("∀", s, errors);
+        let [v, rest] = parseVar(s2, errors);
+        let [e, rest2] = parseQExpr(rest, errors);
+        return [{ op: "all", v, e }, rest2];
+    } else if (s.startsWith("∃")) {
+        let s2 = match("∃", s, errors);
+        let [v, rest] = parseVar(s2, errors);
+        let [e, rest2] = parseQExpr(rest, errors);
+        return [{ op: "exists", v, e }, rest2];
+    } else if (s.startsWith("(∀")) {
+        let s2 = match("(∀", s, errors);
+        let [v, rest] = parseVar(s2, errors);
+        rest = match(")", rest, errors);
+        let [e, rest2] = parseQExpr(rest, errors);
+        return [{ op: "all", v, e }, rest2];
+    } else if (s.startsWith("(∃")) {
+        let s2 = match("(∃", s, errors);
+        let [v, rest] = parseVar(s2, errors);
+        rest = match(")", rest, errors);
+        let [e, rest2] = parseQExpr(rest, errors);
+        return [{ op: "exists", v, e }, rest2];
+    } else if (s.startsWith("not")) {
+        let s2 = match("not", s, errors);
+        let [e, rest] = parseQExpr(s2, errors);
+        return [{ op: "not", e }, rest];
+    } else if (s.startsWith("(")) {
+        let s2 = match("(", s, errors);
+        let [e, rest] = parseExpr(s2, errors);
+        rest = match(")", rest, errors);
+        return [e, rest];
+    } else {
+        return parseTerm(s, errors);
+    }
+}
+
+function parseTerm(s, errors) {
+    if (s.match(/^\w/)) {
+        let [v, rest] = parseVar(s, errors);
+        if (rest.startsWith("(")) {
+            let [args, rest2] = parseArgs(rest, errors);
+            return [{ op: "pred", v, args }, rest2];
+        } else {
+            return [{ op: "prop", v }, rest];
+        }
+    } else if (s.startsWith("⊥")) {
+        let rest = match("⊥", s, errors);
+        return [{ op: "false" }, rest];
+    } else if (s.startsWith("⊤")) {
+        let rest = match("⊤", s, errors);
+        return [{ op: "true" }, rest];
+    } else {
+        errors.push(`Unrecognized term: '${s}'`);
+        return ["", s];
+    }
+}
+
+function parseArgs(s, errors) {
+    let rest = match("(", s, errors);
+    let args = [];
+    if (!rest.startsWith(")")) {
+        let [e, rest2] = parseVar(rest, errors);
+        args.push(e);
+        while (rest2.startsWith(",")) {
+            rest2 = match(",", rest2, errors);
+            let [e2, rest3] = parseVar(rest2, errors);
+            args.push(e2);
+            rest2 = rest3;
+        }
+        rest = rest2;
+    }
+    rest = match(")", rest, errors);
+    return [args, rest];
+}
+
+function parseVar(s, errors) {
+    let m = s.match(/^\w+/);
+    if (m) {
+        let v = m[0];
+        let rest = match(v, s, errors);
+        if (v.endsWith("_") && rest.startsWith("(")) {
+            let m2 = rest.match(/\(\w*\)/);
+            if (m2) {
+                let sub = m2[0].substring(1, m2[0].length - 1);
+                v = v + '{' + sub + '}';
+                rest = s.substring(v.length).trim();
+            }
+        }
+        return [v, rest];
+    } else {
+        errors.push(`Expected identifier: '${s}'`);
+        return ["", s];
+    }
+}
+
+function paren(s, level, min) {
+    if (level > min) {
+        return "(" + s + ")";
+    } else {
+        return s;
+    }
+}
+
+function render(e, level = 0) {
+    if (e.op) {
+        switch (e.op) {
+            case "not":
+                return paren("\\lnot " + render(e.e, 3), level, 3);
+
+            case "implies":
+                return paren(render(e.e1, 1) + "\\rightarrow " + render(e.e2, 0), level, 0);
+
+            case "or":
+                return paren(render(e.e1, 1) + "\\lor " + render(e.e2, 2), level, 1);
+
+            case "and":
+                return paren(render(e.e1, 2) + "\\land " + render(e.e2, 3), level, 2);
+
+            case "all":
+                return paren("\\forall " + e.v + render(e.e, 4), level, 4);
+
+            case "exists":
+                return paren("\\exists " + e.v + render(e.e, 4), level, 4);
+
+            case "true":
+                return "\\top";
+
+            case "false":
+                return "\\bot";
+
+            case "prop":
+                return paren(e.v, level, 3);
+
+            case "pred":
+                return paren(e.v + "(" + e.args + ")", level, 3);
+        }
+    } else {
+        return null;
+    }
+}
+
+customElements.define(
+    "expr-test",
+    class extends HTMLDivElement {
+        constructor() {
+            super();
+            this.classList.add('exprtest');
+        }
+
+        connectedCallback() {
+            const exprField = document.createElement("math-field");
+            this.appendChild(exprField);
+
+            exprField.inlineShortcuts = {
+                ...exprField.inlineShortcuts,
+                ...logicShortcuts,
+            };
+            exprField.value = "P \\land Q";
+        }
+    },
+    { extends: "div" },
+);
